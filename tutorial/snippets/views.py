133aab88b06fe2011a50099f70bf544b7afb21ca
md Nazmul Hasan <farhapartex@gmail.com>
2019-01-25 01:17:46 +0600

    Class based API view has been updatded with mixin

--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -5,49 +5,71 @@ from rest_framework.renderers import JSONRenderer
 from rest_framework.parsers import JSONParser
 from .models import Snippet
 from .serializers import SnippetSerializer
-from rest_framework import status 
+from rest_framework import status , mixins, generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class Snippet_List(APIView):
+
+
+class Snippet_List(generics.ListCreateAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+class Snipper_Details(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+
+
+# class Snippet_List(mixins.ListModelMixin,mixins.CreateModelMixin,generics.GenericAPIView):
+
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
     
-    def get(self,request,format=None):
-        snippets = Snippet.objects.all()
-        serializer = SnippetSerializer(snippets, many=True)
-        return Response(serializer.data)
+#     def get(self,request,*args,**kargs):
+#         return self.list(request, *args, **kwargs)
     
-    def post(self,request,format=None):
-        serializer = SnippetSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+#     def post(self,request,*args,**kargs):
+#         return self.create(request, *args, **kwargs)
     
 
 
-class Snipper_Details(APIView):
+# class Snipper_Details(mixins.RetrieveModelMixin, mixins.UpdateModelMixin,mixins.DestroyModelMixin,
+#                 generics.GenericAPIView):
 
-    def get_object(self,pk):
-        try:
-            snippet = Snippet.objects.get(pk=pk)
-        except snippet.DoesNotExit:
-            return Response(status=status.HTTP_400_NOT_FOUND)
+#     queryset = Snippet.objects.all()
+#     serializer_class = SnippetSerializer
 
-    def get(self,request,pk,format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet)
-        return Response(serializer.data)
+#     def get(self, request, *args, **kwargs):
+#         return self.retrieve(request, *args, **kwargs)
+
+#     def put(self, request, *args, **kwargs):
+#         return self.update(request, *args, **kwargs)
+
+#     def delete(self, request, *args, **kwargs):
+#         return self.destroy(request, *args, **kwargs)
+
+    # def get_object(self,pk):
+    #     try:
+    #         snippet = Snippet.objects.get(pk=pk)
+    #     except snippet.DoesNotExit:
+    #         return Response(status=status.HTTP_400_NOT_FOUND)
+
+    # def get(self,request,pk,format=None):
+    #     snippet = self.get_object(pk)
+    #     serializer = SnippetSerializer(snippet)
+    #     return Response(serializer.data)
     
-    def put(self,request,pk,format=None):
-        snippet = self.get_object(pk)
-        serializer = SnippetSerializer(snippet,data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
-        return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
+    # def put(self,request,pk,format=None):
+    #     snippet = self.get_object(pk)
+    #     serializer = SnippetSerializer(snippet,data=data)
+    #     if serializer.is_valid():
+    #         serializer.save()
+    #         return Response(serializer.data)
+    #     return Response(serializer.error, status=status.HTTP_400_BAD_REQUEST)
     
-    def delete(self,request,pk,format=None):
-        snippet = self.get_object(pk)
-        snippet.delete()
-        return Response(status=status.HTTP_400_BAD_REQUEST)
+    # def delete(self,request,pk,format=None):
+    #     snippet = self.get_object(pk)
+    #     snippet.delete()
+    #     return Response(status=status.HTTP_400_BAD_REQUEST)
